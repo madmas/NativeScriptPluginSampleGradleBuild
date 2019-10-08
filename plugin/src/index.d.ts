7a15de99d50644fbeca9f8b9f3552ac44d24227e
Markus Schlichting <markus.schlichting@karakun.com>
2019-10-08 13:54:26 +0200

    Initial commit of sample plugin build with android

---
+++ plugin/src/index.d.ts
@@ -0,0 +1,6 @@
+import { Common } from './yourplugin.common';
+export declare class Yourplugin extends Common {
+  // define your typings manually
+  // or..
+  // take the ios or android .d.ts files and copy/paste them here
+}
